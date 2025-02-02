baf6ca8ce89ac91b7ebc2e14872fc6950fe2b859
NataljaNeumann <NataljaNeumann@gmx.de>
2025-02-02 11:59:00 +0100

    Added English GUI

--- VokabelTrainer/NewLanguageFile.Designer.cs
+++ VokabelTrainer/NewLanguageFile.Designer.cs
@@ -46,6 +46,7 @@ namespace VokabelTrainer
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewLanguageFile));
             this.m_lblFirstLanguage = new System.Windows.Forms.Label();
             this.m_tbxFirstLanguage = new System.Windows.Forms.TextBox();
             this.m_tbxSecondLanguage = new System.Windows.Forms.TextBox();
@@ -60,128 +61,117 @@ namespace VokabelTrainer
             // 
             // m_lblFirstLanguage
             // 
-            this.m_lblFirstLanguage.AutoSize = true;
-            this.m_lblFirstLanguage.Location = new System.Drawing.Point(16, 11);
-            this.m_lblFirstLanguage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.m_lblFirstLanguage.AccessibleDescription = null;
+            this.m_lblFirstLanguage.AccessibleName = null;
+            resources.ApplyResources(this.m_lblFirstLanguage, "m_lblFirstLanguage");
+            this.m_lblFirstLanguage.Font = null;
             this.m_lblFirstLanguage.Name = "m_lblFirstLanguage";
-            this.m_lblFirstLanguage.Size = new System.Drawing.Size(96, 16);
-            this.m_lblFirstLanguage.TabIndex = 0;
-            this.m_lblFirstLanguage.Text = "Erste Sprache:";
             // 
             // m_tbxFirstLanguage
             // 
-            this.m_tbxFirstLanguage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.m_tbxFirstLanguage.Location = new System.Drawing.Point(20, 31);
-            this.m_tbxFirstLanguage.Margin = new System.Windows.Forms.Padding(4);
+            this.m_tbxFirstLanguage.AccessibleDescription = null;
+            this.m_tbxFirstLanguage.AccessibleName = null;
+            resources.ApplyResources(this.m_tbxFirstLanguage, "m_tbxFirstLanguage");
+            this.m_tbxFirstLanguage.BackgroundImage = null;
+            this.m_tbxFirstLanguage.Font = null;
             this.m_tbxFirstLanguage.Name = "m_tbxFirstLanguage";
-            this.m_tbxFirstLanguage.Size = new System.Drawing.Size(481, 22);
-            this.m_tbxFirstLanguage.TabIndex = 1;
             this.m_tbxFirstLanguage.TextChanged += new System.EventHandler(this.textBoxFirstLanguage_TextChanged);
             // 
             // m_tbxSecondLanguage
             // 
-            this.m_tbxSecondLanguage.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.m_tbxSecondLanguage.Location = new System.Drawing.Point(20, 95);
-            this.m_tbxSecondLanguage.Margin = new System.Windows.Forms.Padding(4);
+            this.m_tbxSecondLanguage.AccessibleDescription = null;
+            this.m_tbxSecondLanguage.AccessibleName = null;
+            resources.ApplyResources(this.m_tbxSecondLanguage, "m_tbxSecondLanguage");
+            this.m_tbxSecondLanguage.BackgroundImage = null;
+            this.m_tbxSecondLanguage.Font = null;
             this.m_tbxSecondLanguage.Name = "m_tbxSecondLanguage";
-            this.m_tbxSecondLanguage.Size = new System.Drawing.Size(481, 22);
-            this.m_tbxSecondLanguage.TabIndex = 3;
-            this.m_tbxSecondLanguage.Text = "Deutsch";
             this.m_tbxSecondLanguage.TextChanged += new System.EventHandler(this.textBoxSecondLanguage_TextChanged);
             // 
             // m_lblSecondLanguage
             // 
-            this.m_lblSecondLanguage.AutoSize = true;
-            this.m_lblSecondLanguage.Location = new System.Drawing.Point(17, 71);
-            this.m_lblSecondLanguage.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.m_lblSecondLanguage.AccessibleDescription = null;
+            this.m_lblSecondLanguage.AccessibleName = null;
+            resources.ApplyResources(this.m_lblSecondLanguage, "m_lblSecondLanguage");
+            this.m_lblSecondLanguage.Font = null;
             this.m_lblSecondLanguage.Name = "m_lblSecondLanguage";
-            this.m_lblSecondLanguage.Size = new System.Drawing.Size(104, 16);
-            this.m_lblSecondLanguage.TabIndex = 2;
-            this.m_lblSecondLanguage.Text = "Zweite Sprache:";
             // 
             // m_btnCreateLanguageFile
             // 
-            this.m_btnCreateLanguageFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.m_btnCreateLanguageFile.Enabled = false;
-            this.m_btnCreateLanguageFile.Location = new System.Drawing.Point(295, 177);
-            this.m_btnCreateLanguageFile.Margin = new System.Windows.Forms.Padding(4);
+            this.m_btnCreateLanguageFile.AccessibleDescription = null;
+            this.m_btnCreateLanguageFile.AccessibleName = null;
+            resources.ApplyResources(this.m_btnCreateLanguageFile, "m_btnCreateLanguageFile");
+            this.m_btnCreateLanguageFile.BackgroundImage = null;
+            this.m_btnCreateLanguageFile.Font = null;
             this.m_btnCreateLanguageFile.Name = "m_btnCreateLanguageFile";
-            this.m_btnCreateLanguageFile.Size = new System.Drawing.Size(100, 28);
-            this.m_btnCreateLanguageFile.TabIndex = 6;
-            this.m_btnCreateLanguageFile.Text = "Erstellen";
             this.m_btnCreateLanguageFile.UseVisualStyleBackColor = true;
             this.m_btnCreateLanguageFile.Click += new System.EventHandler(this.button1_Click);
             // 
             // m_btnCancel
             // 
-            this.m_btnCancel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.m_btnCancel.AccessibleDescription = null;
+            this.m_btnCancel.AccessibleName = null;
+            resources.ApplyResources(this.m_btnCancel, "m_btnCancel");
+            this.m_btnCancel.BackgroundImage = null;
             this.m_btnCancel.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.m_btnCancel.Location = new System.Drawing.Point(403, 177);
-            this.m_btnCancel.Margin = new System.Windows.Forms.Padding(4);
+            this.m_btnCancel.Font = null;
             this.m_btnCancel.Name = "m_btnCancel";
-            this.m_btnCancel.Size = new System.Drawing.Size(100, 28);
-            this.m_btnCancel.TabIndex = 7;
-            this.m_btnCancel.Text = "Abbrechen";
             this.m_btnCancel.UseVisualStyleBackColor = true;
             this.m_btnCancel.Click += new System.EventHandler(this.button2_Click);
             // 
             // m_chkLanguageFileUnderGPL2
             // 
-            this.m_chkLanguageFileUnderGPL2.AutoSize = true;
+            this.m_chkLanguageFileUnderGPL2.AccessibleDescription = null;
+            this.m_chkLanguageFileUnderGPL2.AccessibleName = null;
+            resources.ApplyResources(this.m_chkLanguageFileUnderGPL2, "m_chkLanguageFileUnderGPL2");
+            this.m_chkLanguageFileUnderGPL2.BackgroundImage = null;
             this.m_chkLanguageFileUnderGPL2.Checked = true;
             this.m_chkLanguageFileUnderGPL2.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.m_chkLanguageFileUnderGPL2.Location = new System.Drawing.Point(19, 136);
+            this.m_chkLanguageFileUnderGPL2.Font = null;
             this.m_chkLanguageFileUnderGPL2.Name = "m_chkLanguageFileUnderGPL2";
-            this.m_chkLanguageFileUnderGPL2.Size = new System.Drawing.Size(251, 20);
-            this.m_chkLanguageFileUnderGPL2.TabIndex = 4;
-            this.m_chkLanguageFileUnderGPL2.Text = "Sprachdatei unter GPL2-Lizenz stellen";
             this.m_chkLanguageFileUnderGPL2.UseVisualStyleBackColor = true;
             this.m_chkLanguageFileUnderGPL2.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
             // 
             // m_chkLanguageFileModifiable
             // 
-            this.m_chkLanguageFileModifiable.AutoSize = true;
+            this.m_chkLanguageFileModifiable.AccessibleDescription = null;
+            this.m_chkLanguageFileModifiable.AccessibleName = null;
+            resources.ApplyResources(this.m_chkLanguageFileModifiable, "m_chkLanguageFileModifiable");
+            this.m_chkLanguageFileModifiable.BackgroundImage = null;
             this.m_chkLanguageFileModifiable.Checked = true;
             this.m_chkLanguageFileModifiable.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.m_chkLanguageFileModifiable.Enabled = false;
-            this.m_chkLanguageFileModifiable.Location = new System.Drawing.Point(19, 162);
+            this.m_chkLanguageFileModifiable.Font = null;
             this.m_chkLanguageFileModifiable.Name = "m_chkLanguageFileModifiable";
-            this.m_chkLanguageFileModifiable.Size = new System.Drawing.Size(180, 20);
-            this.m_chkLanguageFileModifiable.TabIndex = 5;
-            this.m_chkLanguageFileModifiable.Text = "Sprachdatei modifizierbar";
             this.m_chkLanguageFileModifiable.UseVisualStyleBackColor = true;
             // 
             // m_chkFirstLanguageRTL
             // 
-            this.m_chkFirstLanguageRTL.AutoSize = true;
-            this.m_chkFirstLanguageRTL.Location = new System.Drawing.Point(193, 10);
+            this.m_chkFirstLanguageRTL.AccessibleDescription = null;
+            this.m_chkFirstLanguageRTL.AccessibleName = null;
+            resources.ApplyResources(this.m_chkFirstLanguageRTL, "m_chkFirstLanguageRTL");
+            this.m_chkFirstLanguageRTL.BackgroundImage = null;
+            this.m_chkFirstLanguageRTL.Font = null;
             this.m_chkFirstLanguageRTL.Name = "m_chkFirstLanguageRTL";
-            this.m_chkFirstLanguageRTL.Size = new System.Drawing.Size(209, 20);
-            this.m_chkFirstLanguageRTL.TabIndex = 8;
-            this.m_chkFirstLanguageRTL.Text = "Schreibweise rechts nach links";
             this.m_chkFirstLanguageRTL.UseVisualStyleBackColor = true;
-            this.m_chkFirstLanguageRTL.Visible = false;
             // 
             // m_chkSecondLanguageRTL
             // 
-            this.m_chkSecondLanguageRTL.AutoSize = true;
-            this.m_chkSecondLanguageRTL.Location = new System.Drawing.Point(193, 71);
+            this.m_chkSecondLanguageRTL.AccessibleDescription = null;
+            this.m_chkSecondLanguageRTL.AccessibleName = null;
+            resources.ApplyResources(this.m_chkSecondLanguageRTL, "m_chkSecondLanguageRTL");
+            this.m_chkSecondLanguageRTL.BackgroundImage = null;
+            this.m_chkSecondLanguageRTL.Font = null;
             this.m_chkSecondLanguageRTL.Name = "m_chkSecondLanguageRTL";
-            this.m_chkSecondLanguageRTL.Size = new System.Drawing.Size(209, 20);
-            this.m_chkSecondLanguageRTL.TabIndex = 9;
-            this.m_chkSecondLanguageRTL.Text = "Schreibweise rechts nach links";
             this.m_chkSecondLanguageRTL.UseVisualStyleBackColor = true;
-            this.m_chkSecondLanguageRTL.Visible = false;
             // 
             // NewLanguageFile
             // 
             this.AcceptButton = this.m_btnCreateLanguageFile;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AccessibleDescription = null;
+            this.AccessibleName = null;
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = null;
             this.CancelButton = this.m_btnCancel;
-            this.ClientSize = new System.Drawing.Size(519, 220);
             this.Controls.Add(this.m_chkSecondLanguageRTL);
             this.Controls.Add(this.m_chkFirstLanguageRTL);
             this.Controls.Add(this.m_chkLanguageFileModifiable);
@@ -192,14 +182,12 @@ namespace VokabelTrainer
             this.Controls.Add(this.m_tbxSecondLanguage);
             this.Controls.Add(this.m_tbxFirstLanguage);
             this.Controls.Add(this.m_lblFirstLanguage);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Icon = null;
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "NewLanguageFile";
             this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
-            this.Text = "Neue Sprachdatei";
             this.ResumeLayout(false);
             this.PerformLayout();
 
