6eac303ddfca36d99bfed40ee73222d23dbb52bc
NataljaNeumann <NataljaNeumann@gmx.de>
2025-02-02 11:58:49 +0100

    Added English GUI

--- VokabelTrainer/NewMessageBox.Designer.cs
+++ VokabelTrainer/NewMessageBox.Designer.cs
@@ -46,48 +46,44 @@ namespace VokabelTrainer
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(NewMessageBox));
             this.m_lblMessageText = new System.Windows.Forms.Label();
             this.m_btnOK = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // m_lblMessageText
             // 
-            this.m_lblMessageText.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
-                        | System.Windows.Forms.AnchorStyles.Left)
-                        | System.Windows.Forms.AnchorStyles.Right)));
-            this.m_lblMessageText.Location = new System.Drawing.Point(16, 11);
-            this.m_lblMessageText.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.m_lblMessageText.AccessibleDescription = null;
+            this.m_lblMessageText.AccessibleName = null;
+            resources.ApplyResources(this.m_lblMessageText, "m_lblMessageText");
+            this.m_lblMessageText.Font = null;
             this.m_lblMessageText.Name = "m_lblMessageText";
-            this.m_lblMessageText.Size = new System.Drawing.Size(382, 87);
-            this.m_lblMessageText.TabIndex = 0;
-            this.m_lblMessageText.Text = "label1";
             this.m_lblMessageText.SizeChanged += new System.EventHandler(this.label1_SizeChanged);
             // 
             // m_btnOK
             // 
-            this.m_btnOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.m_btnOK.Location = new System.Drawing.Point(295, 119);
+            this.m_btnOK.AccessibleDescription = null;
+            this.m_btnOK.AccessibleName = null;
+            resources.ApplyResources(this.m_btnOK, "m_btnOK");
+            this.m_btnOK.BackgroundImage = null;
+            this.m_btnOK.Font = null;
             this.m_btnOK.Name = "m_btnOK";
-            this.m_btnOK.Size = new System.Drawing.Size(103, 26);
-            this.m_btnOK.TabIndex = 1;
-            this.m_btnOK.Text = "OK";
             this.m_btnOK.UseVisualStyleBackColor = true;
             this.m_btnOK.Click += new System.EventHandler(this.button1_Click);
             // 
             // NewMessageBox
             // 
             this.AcceptButton = this.m_btnOK;
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AccessibleDescription = null;
+            this.AccessibleName = null;
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(410, 157);
+            this.BackgroundImage = null;
             this.Controls.Add(this.m_btnOK);
             this.Controls.Add(this.m_lblMessageText);
-            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
-            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Icon = null;
             this.Name = "NewMessageBox";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Vokabel-Trainer";
             this.Shown += new System.EventHandler(this.NewMessageBox_Shown);
             this.ResumeLayout(false);
 
