feb431fbffd1894d7435a1691ff1ae18c813561f
NataljaNeumann <NataljaNeumann@gmx.de>
2025-02-06 21:04:03 +0100

    Added steps for transforming strings to csv and back

---
+++ IntegrateTranslationCSVs.cmd
@@ -0,0 +1,7 @@
+ResxVsCsv --directory VokabelTrainer --toresx WordTest.csv 
+ResxVsCsv --directory VokabelTrainer --toresx NewDictionaryPair.csv 
+ResxVsCsv --directory VokabelTrainer --toresx NewLanguageFile.csv
+ResxVsCsv --directory VokabelTrainer --toresx NewMessageBox.csv
+ResxVsCsv --directory VokabelTrainer --toresx VokabelTrainer.csv 
+ResxVsCsv --directory VokabelTrainer\Properties --toresx Resources.csv 
+pause
