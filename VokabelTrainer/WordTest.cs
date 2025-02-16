cbccad50f0e093342f5740c951169bb8b9af63d8
NataljaNeumann <NataljaNeumann@gmx.de>
2025-02-16 19:17:47 +0100

    Worked on code style

--- VokabelTrainer/WordTest.cs
+++ VokabelTrainer/WordTest.cs
@@ -23,34 +23,83 @@ using System.Windows.Forms;
 
 namespace VokabelTrainer
 {
+    //*******************************************************************************************************
+    /// <summary>
+    /// Form for testing single words
+    /// </summary>
+    //*******************************************************************************************************
     public partial class WordTest : Form
     {
+        //===================================================================================================
+        /// <summary>
+        /// Constructs a new word test object
+        /// </summary>
+        //===================================================================================================
         public WordTest()
         {
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
+
+        //===================================================================================================
+        /// <summary>
+        /// This is executed when text has changed in text box
+        /// </summary>
+        /// <param name="oSender">Sender object</param>
+        /// <param name="oEventArgs">Event args</param>
+        //===================================================================================================
+        private void textBox1_TextChanged(object oSender, EventArgs oEventArgs)
         {
             m_btnNext.Enabled = m_btnLast.Enabled = m_tbxAskedTranslation.Text.Length > 0;
         }
 
-        private void buttonNext_Click(object sender, EventArgs e)
+
+        //===================================================================================================
+        /// <summary>
+        /// This is executed when 'next' button is clicked
+        /// </summary>
+        /// <param name="oSender">Sender object</param>
+        /// <param name="oEventArgs">Event args</param>
+        //===================================================================================================
+        private void buttonNext_Click(object oSender, EventArgs oEventArgs)
         {
             DialogResult = DialogResult.Retry;
         }
 
-        private void buttonLast_Click(object sender, EventArgs e)
+        //===================================================================================================
+        /// <summary>
+        /// This is executed when 'last' button is clicked
+        /// </summary>
+        /// <param name="oSender">Sender object</param>
+        /// <param name="oEventArgs">Event args</param>
+        //===================================================================================================
+        private void buttonLast_Click(object oSender, EventArgs oEventArgs)
         {
             DialogResult = DialogResult.OK;
         }
 
-        private void buttonCancel_Click(object sender, EventArgs e)
+
+        //===================================================================================================
+        /// <summary>
+        /// This is executed when 'cancel' button is clicked
+        /// </summary>
+        /// <param name="oSender">Sender object</param>
+        /// <param name="oEventArgs">Event args</param>
+        //===================================================================================================
+        private void buttonCancel_Click(object oSender, EventArgs oEventArgs)
         {
             DialogResult = DialogResult.Cancel;
         }
 
-        private void WordTest_Shown(object sender, EventArgs e)
+
+        //===================================================================================================
+        /// <summary>
+        /// This is executed when the form is shown
+        /// </summary>
+        /// <param name="oSender">Sender object</param>
+        /// <param name="oEventArgs">Event args</param>
+        //===================================================================================================
+        private void WordTest_Shown(object oSender, EventArgs oEventArgs)
         {
 
         }
