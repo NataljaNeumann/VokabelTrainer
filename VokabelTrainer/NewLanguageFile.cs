3fcab984e32147aa6032fa133049bb6433249a54
NataljaNeumann <NataljaNeumann@gmx.de>
2025-02-16 20:31:13 +0100

    Updated copyright notice

--- VokabelTrainer/NewLanguageFile.cs
+++ VokabelTrainer/NewLanguageFile.cs
@@ -1,5 +1,5 @@
-﻿// Vokabel-Trainer v1.2
-// Copyright (C) 2019 NataljaNeumann@gmx.de
+﻿// VokabelTrainer v1.2
+// Copyright (C) 2019-2025 NataljaNeumann@gmx.de
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
--- VokabelTrainer/Speaker.cs
+++ VokabelTrainer/Speaker.cs
@@ -1,20 +1,19 @@
-﻿/*  Vokabel-Trainer aims to provide you a simple vocabulary training app
-    Copyright (C) 2024 NataljaNeumann@gmx.de
-
-    This program is free software; you can redistribute it and/or modify
-    it under the terms of the GNU General Public License as published by
-    the Free Software Foundation; either version 2 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU General Public License for more details.
-
-    You should have received a copy of the GNU General Public License along
-    with this program; if not, write to the Free Software Foundation, Inc.,
-    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
-*/
+﻿// VokabelTrainer v1.2
+// Copyright (C) 2019-2025 NataljaNeumann@gmx.de
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
 using System;
 using System.Collections.Generic;
