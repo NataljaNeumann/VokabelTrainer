4ecb0cd8b5b86088e89dc1fe01f0d08cda38746e
NataljaNeumann <NataljaNeumann@users.noreply.github.com>
2025-10-25 13:23:45 +0000

    Set version to 1.5.10.0

--- VokabelTrainer/Properties/AssemblyInfo.cs
+++ VokabelTrainer/Properties/AssemblyInfo.cs
@@ -50,6 +50,6 @@ using System.Resources;
 // Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.5.9.0")]
-[assembly: AssemblyFileVersion("1.5.9.0")]
+[assembly: AssemblyVersion("1.5.10.0")]
+[assembly: AssemblyFileVersion("1.5.10.0")]
 [assembly: NeutralResourcesLanguageAttribute("")]
